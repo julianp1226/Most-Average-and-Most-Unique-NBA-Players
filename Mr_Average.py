2a2fab49f69152c3c275ea93e79791628161ae2c
Julian Perez <98099214+julianp1226@users.noreply.github.com>
2022-09-12 18:17:42 -0400

    Update Mr_Average.py

--- Mr_Average.py
+++ Mr_Average.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 while True:
     # getting user input on year they want to look at
@@ -13,47 +14,15 @@ while True:
         # found the mean of all the stats in the dataset and appends it to the bottom
         df.loc['Average'] = df.mean(numeric_only=True)
 
-        # outputs list of top 10 scorers in the league
-        def top_scorer():
-            highest_scoring = df[df["MP"] > 15].sort_values(
-                "PTS", ascending=False).head(10)
-            print(highest_scoring)
+        # adding the columns for the difference between the players' averages and the average of that stat across the league
+        for column_name in df.select_dtypes([np.number]).columns:
+            df["Diff" +
+                column_name] = abs(df[column_name] - df[column_name]['Average'])
 
-        # outputs the most average scorer PPG-wise in the league
-        def mr_AverageScorer():
-            average_scoring = df[df["MP"] > 15].sort_values(
-                "DiffPTS", ascending=True).head(1)
-            print(average_scoring["Player"])
+        # dropping difference in year, since it's the same for all players
+        df.drop("DiffYear", inplace=True, axis=1)
 
-        # calculating the differences in each players stat and the average of that stat across the league
-        df['DiffAge'] = abs(df["Age"] - df["Age"]['Average'])
-        df['DiffG'] = abs(df["G"] - df["G"]['Average'])
-        df['DiffGS'] = abs(df["GS"] - df["GS"]['Average'])
-        df['DiffMP'] = abs(df["MP"] - df["MP"]['Average'])
-        df['DiffFG'] = abs(df["FG"] - df["FG"]['Average'])
-        df['DiffFGA'] = abs(df["FGA"] - df["FGA"]['Average'])
-        df['DiffFG%'] = abs(df["FG%"] - df["FG%"]['Average'])
-        df['Diff3P'] = abs(df["3P"] - df["3P"]['Average'])
-        df['Diff3PA'] = abs(df["3PA"] - df["3PA"]['Average'])
-        df['Diff3P%'] = abs(df["3P%"] - df["3P%"]['Average'])
-        df['Diff2P'] = abs(df["2P"] - df["2P"]['Average'])
-        df['Diff2PA'] = abs(df["2PA"] - df["2PA"]['Average'])
-        df['Diff2P%'] = abs(df["2P%"] - df["2P%"]['Average'])
-        df['DiffeFG%'] = abs(df["eFG%"] - df["eFG%"]['Average'])
-        df['DiffFT'] = abs(df["FT"] - df["FT"]['Average'])
-        df['DiffFTA'] = abs(df["FTA"] - df["FTA"]['Average'])
-        df['DiffFT%'] = abs(df["FT%"] - df["FT%"]['Average'])
-        df['DiffORB'] = abs(df["ORB"] - df["ORB"]['Average'])
-        df['DiffDRB'] = abs(df["DRB"] - df["DRB"]['Average'])
-        df['DiffTRB'] = abs(df["TRB"] - df["TRB"]['Average'])
-        df['DiffAST'] = abs(df["AST"] - df["AST"]['Average'])
-        df['DiffSTL'] = abs(df["STL"] - df["STL"]['Average'])
-        df['DiffBLK'] = abs(df["BLK"] - df["BLK"]['Average'])
-        df['DiffTOV'] = abs(df["TOV"] - df["TOV"]['Average'])
-        df['DiffPF'] = abs(df["PF"] - df["PF"]['Average'])
-        df['DiffPTS'] = abs(df["PTS"] - df["PTS"]['Average'])
-
-        # calculating the total difference score of each player
+        # adding up all the difference columns to get a total difference score
         df["TotalDiff"] = df["DiffAge"] + df["DiffG"] + df["DiffGS"] + df["DiffMP"] + df["DiffFG"] + df["DiffFG%"] + df["Diff3P"] + df["Diff3PA"] + df["Diff3P%"] + df["Diff2P"] + df["Diff2PA"] + df["Diff2P%"] + \
             df["DiffeFG%"] + df["DiffFT"] + df["DiffFTA"] + df["DiffFT%"] + df["DiffORB"] + df["DiffDRB"] + \
             df["DiffTRB"] + df["DiffAST"] + df["DiffSTL"] + \
@@ -74,6 +43,18 @@ while True:
             average2 = average.drop(
                 'Average').loc[:, average.columns != "Unnamed: 0"]
             print(average2.head(10))
+
+         # outputs list of top 10 scorers in the league
+        def top_scorer():
+            highest_scoring = df[df["MP"] > 15].sort_values(
+                "PTS", ascending=False).head(10)
+            print(highest_scoring)
+
+        # outputs the most average scorer PPG-wise in the league
+        def mr_AverageScorer():
+            average_scoring = df[df["MP"] > 15].sort_values(
+                "DiffPTS", ascending=True).head(1)
+            print(average_scoring["Player"])
 
         # create function map to allow function call on user input
         func_map = {'a': mr_Average, 'u': mr_Unique}
